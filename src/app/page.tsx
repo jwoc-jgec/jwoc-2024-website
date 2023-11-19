4e44e64f427ea6c9d596a84a9dd221f1d96a0674
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-11-19 22:01:03 +0530

    added navbar and made minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Orbitron } from "next/font/google";
 import Link from "next/link";
-
-const orbitron = Orbitron({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`${orbitron.className}  flex min-h-screen flex-col items-center justify-between py-10 px-5 lg:px-0 md:flex-row md:gap-64`}
+      className={` flex max-w-7xl mx-auto flex-col justify-between py-10 pt-16 px-5 md:flex-row`}
     >
       <div className="flex text-center md:text-start flex-col text-[#00FFFF]">
         <div className="pb-14">
@@ -16,7 +13,7 @@ export default function Home() {
           <h1 className="text-4xl md:text-6xl font-bold text-white drop-shadow-glow">
             A MONTH OF
           </h1>
-          <h2 className="text-5xl md:text-7xl font-extrabold tracking-wide drop-shadow-glow">
+          <h2 className="text-5xl md:text-7xl font-extrabold tracking-wide drop-shadow-cyanGlow">
             Open Source
           </h2>
         </div>
@@ -24,22 +21,56 @@ export default function Home() {
           Open Source contribution. By JGEC. For all. Plunge into Open Source
           contribution. By JGEC. For all.
         </p>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center md:items-start">
           <Link
             href={""}
-            className="relative flex items-center justify-center drop-shadow-glow py-3 px-10 md:py-5 md:px-20 bg-cyan-300 bg-opacity-10 font-bold text-lg md:text-2xl ring-2 ring-cyan-300"
+            className="relative flex items-center justify-center drop-shadow-cyanGlow py-3 px-10 md:py-5 md:px-28 bg-cyan-300 bg-opacity-10 font-bold text-lg md:text-2xl ring-2 ring-cyan-300"
           >
             Register as mentee{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide mt-1 ml-1 lucide-chevron-right"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
           </Link>
-          <Link href={""} className="underline underline-offset-4 pt-5">
+          <Link
+            href={""}
+            className="underline flex items-end underline-offset-4 pt-5"
+          >
             I want to become a mentor
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 24 16"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-arrow-up-right"
+            >
+              <path d="M7 7h10v10" />
+              <path d="M7 17 17 7" />
+            </svg>
           </Link>
         </div>
       </div>
-      <div className="flex items-center justify-center p-10">
-        <div className="w-96 h-96 rounded-full bg-cyan-300 absolute z-0 blur-3xl opacity-30" />
+      <div className="flex items-center justify-center">
+        <div className="w-[30rem] h-[30rem] rounded-full bg-cyan-500 z-0 blur-3xl opacity-20" />
 
-        <div className="text-8xl  text-white absolute z-10">05</div>
+        <div className="text-8xl  text-white drop-shadow-glow absolute z-10">
+          05
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,19 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       dropShadow: {
         glow: [
-          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 60px rgba(255,255, 255, 0.75)",
           "0 0px 65px rgba(255, 255,255, 0.2)",
+        ],
+        cyanGlow: [
+          "0 0px 60px rgba(0,255, 255, 0.75)",
+          "0 0px 65px rgba(0, 255,255, 0.60)",
         ],
       },
     },
