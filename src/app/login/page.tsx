f594bf3a57d929a8cbfdba9f625c4c5e07968e8a
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-11-25 17:40:15 +0530

    fixed build fail

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,16 @@
-import LoginForm from '@/components/LoginForm'
-import { getServerSession } from 'next-auth';
-import React from 'react'
-import { authOptions } from '../api/auth/[...nextauth]/route';
-import { redirect } from 'next/navigation';
-
+import LoginForm from "@/components/LoginForm";
+import { getServerSession } from "next-auth";
+import React from "react";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/utils/authOptions";
 
 export default async function login() {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (session) return redirect("/profile");
+  if (session) return redirect("/profile");
   return (
-    <div><LoginForm/></div>
-  )
+    <div>
+      <LoginForm />
+    </div>
+  );
 }
